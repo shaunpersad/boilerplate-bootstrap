8e57c964daadff96cd8767e922b70cf0dfa07d73
shaunpersad <shaunpersad@gmail.com>
2014-05-03 21:40:41 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+                     
+        <!-- Bootstrap -->
+        <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->       
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"> 
+        
+        <!-- jQueryUI -->
+		<!--<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />-->  
+                
+        <!-- Fancybox -->       
+        <!--<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css">-->
+        
+        <!-- application css -->
+        <link rel="stylesheet" href="css/main.css">
+        
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+
+        <!-- jQuery -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <!-- Bootstrap -->
+        <!--<script src="js/vendor/bootstrap.min.js"></script>-->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+        <!-- jQueryUI -->
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+		<!-- Form -->
+        <script src="js/form/jquery.form.min.js"></script>
+
+		<!-- Validate -->
+        <!--<script src="js/validate/jquery.validate.min.js"></script>-->
+        <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+
+		<!-- Fancybox -->
+        <script src="js/fancybox/jquery.fancybox.pack.js"></script>
+
+		<!-- SlidesJS -->
+        <script src="js/slidesjs/jquery.slides.min.js"></script>
+
+		<!-- imagesloaded -->
+        <script src="js/imagesloaded/imagesloaded.pkgd.min.js"></script>
+
+		<!-- application javascript -->
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
